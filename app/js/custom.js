180eca54f1538d8874ccc0bd23813a3f4c1a6d9e
knazark <knazark@tenantcloud.com>
2018-08-22 23:01:40 +0300

    fix: Header

--- app/js/custom.js
+++ app/js/custom.js
@@ -104,18 +104,20 @@
 
 	// Header Scrolling Set White Background
 	$(window).on('scroll', function() {
-		var width = $(window).width();
-		if(width > 991) {
-			var scroll = $(window).scrollTop();
-			if (scroll >= 30) {
-				$(".header-area").addClass("header-sticky");
-				$(".header-area .dark-logo").css('display', 'block');
-				$(".header-area .light-logo").css('display', 'none');
-			}else{
-				$(".header-area").removeClass("header-sticky");
-				$(".header-area .dark-logo").css('display', 'none');
-				$(".header-area .light-logo").css('display', 'block');
-			}
+		// var width = $(window).width();
+		// if(width > 991) {
+		//
+		// }
+
+		var scroll = $(window).scrollTop();
+		if (scroll >= 30) {
+			$(".header-area").addClass("header-sticky");
+			$(".header-area .dark-logo").css('display', 'block');
+			$(".header-area .light-logo").css('display', 'none');
+		}else{
+			$(".header-area").removeClass("header-sticky");
+			$(".header-area .dark-logo").css('display', 'none');
+			$(".header-area .light-logo").css('display', 'block');
 		}
 	});
 	
